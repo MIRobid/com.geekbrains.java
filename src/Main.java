87d424f66cbc9c407f04ef53e115b047df6060fa
MIRobid <mirobid_i@mail.ru>
2020-11-25 20:31:28 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,54 @@ public class Main {
 
     static final int SIZE=10000000;
     static final int HALF=SIZE/2;
+    static final int FILLER=1;
+    static final int PART_ONE=0;
+    static final int PART_TWO=1;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        float[] arr=new float[SIZE];
-        float[] a1=new float[SIZE];
-        float[] a2=new float[SIZE];
+        System.out.printf("method1: %d\n", doMethod1());
+        System.out.printf("method2: %d\n", doMethod2());
+    }
 
-        for(int i=0;i<arr.length;i++){
-            arr[i]=1;
+    private static long doMethod1() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
         }
-        long start=System.currentTimeMillis();
-        for(int i=0;i<arr.length;i++){
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        long finish=System.currentTimeMillis();
-        System.out.println(finish-start);
+        long finish = System.currentTimeMillis();
+        return finish - start;
+    }
 
-        System.arraycopy(arr,0,a1,0,HALF);
-        System.arraycopy(arr,HALF,a2,0,HALF);
-
+    private static long doMethod2() throws InterruptedException{
+        float[] a=new float[SIZE];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 1;
+        }
         long start1=System.currentTimeMillis();
-        for(int i=0;i<a1.length;i++){
-            a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        float[][] a1=new float[][]{new float[HALF],new float[HALF]};
+        System.arraycopy(a,0,a1[PART_ONE],0,HALF);
+        System.arraycopy(a,HALF,a1[PART_TWO],0,HALF);
+
+        Thread thread = new Thread(()-> {
+            for (int i = 0; i < a1[PART_TWO].length; i++) {
+                a1[PART_TWO][i] = (float) (a1[PART_TWO][i] * Math.sin(0.2f + (i+HALF) / 5) * Math.cos(0.2f + (i+HALF) / 5) * Math.cos(0.4f + (i+HALF) / 2));
+            }
+        });
+        thread.start();
+        for (int i = 0; i < a1[PART_ONE].length; i++) {
+            a1[PART_ONE][i] = (float) (a1[PART_ONE][i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
+        thread.join();
 
-        System.arraycopy(a1,0,arr,0,HALF);
-        System.arraycopy(a2,HALF,arr,HALF,HALF);
-
+        System.arraycopy(a1[PART_ONE],0,a,0,HALF);
+        System.arraycopy(a1[PART_TWO],0,a,HALF,HALF);
         long finish1=System.currentTimeMillis();
-        System.out.println(finish1-start1);
-
+        return finish1-start1;
     }
 }
